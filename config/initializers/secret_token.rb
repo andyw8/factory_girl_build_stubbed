c7bcecc9bfd763ae2e9eef777c09598245f0bf38
Andy Waite <github.aw@andywaite.com>
2013-04-01 23:14:23 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FactoryGirlBuildStubbed::Application.config.secret_token = 'df4178f5742df641990c609c0c4559146793e24a032f055530f6a91ddf708c1a064619d204086ee922d336cd6aeebfd7dd181af1f4e067b3135acadc4d0fd715'
